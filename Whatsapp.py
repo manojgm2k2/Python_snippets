cb43421bccb2e9da19cbffe2f2b8486bb5a75f56
manojgm2k2 <manojgm2k2@gmail.com>
2024-07-30 20:15:47 +0530

    Add files via upload
    In this Python file, import the Pywhatkit python module.
    Then Using the 'Whatsapp msg' Function, Send scheduled Msg to the Number

---
+++ Whatsapp.py
@@ -0,0 +1,5 @@
+# python is an high level  interpretered - Compiled languauge which is used in many industries like ai , ml ,Dat science and many more 
+
+import pywhatkit
+pywhatkit.sendwhatmsg('+91 9972997040','Day 00',20,10)
+
